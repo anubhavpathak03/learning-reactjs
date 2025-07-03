f0a72bef01e93656cd3b70497afac78456fb4dc3
Anubhav Pathak <pathak.anubhavdtu@gmail.com>
2025-07-03 14:40:27 +0530

    learn about creating vite app

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,14 @@
+
+import Youtube from "./Youtube"
+
+function App() {
+  const username = "Anubhav";
+  return (
+    <>
+      <h1>Hello From Vite React App {username}</h1>
+      <Youtube/>
+    </>
+  )
+}
+
+export default App
