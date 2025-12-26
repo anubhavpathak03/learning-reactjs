8ef473146b0297ca03da80f72e05d14a5ca1f105
Anubhav Pathak <pathak.anubhavdtu@gmail.com>
2025-12-26 07:18:01 +0530

    update changes

--- 10TodoContextLocalProject/src/contexts/ToDoContext.js
+++ 10TodoContextLocalProject/src/contexts/ToDoContext.js
@@ -10,7 +10,7 @@ export const ToDoContext = createContext({
         }
     ],
     addTodo: (todo) => {},
-    deleteTodo: (todo) => {},
+    deleteTodo: (id) => {},
     updateTodo: (id, todo) => {},
     toggleComplete: (id) => {}
 })
